236e20329be34bf1cc730f1cfb74689d8c423059
TGP17 <86961834+TGP17@users.noreply.github.com>
2024-06-13 21:27:40 +0200

    Create android.sh

---
+++ .ci/android.sh
@@ -0,0 +1,1 @@
+cmake -GNinja -Bbuild -H. -DCMAKE_TOOLCHAIN_FILE=${ANDROID_NDK_HOME}/build/cmake/android.toolchain.cmake -DANDROID_ABI=arm64-v8a -DANDROID_PLATFORM=android-29 -DANDROID_SDK=${ANDROID_SDK_HOME} -DANDROID_NDK=${ANDROID_NDK_HOME} -DANDROID_ABIS=arm64-v8a && (build && cmake --build . -v || ..)
